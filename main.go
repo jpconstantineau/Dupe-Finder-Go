c3b13adf50b8e8bf5d7467509c35b181c0bc43c8
Pierre Constantineau <jpconstantineau@gmail.com>
2023-05-23 12:19:42 -0600

    added initial code

--- main.go
+++ main.go
@@ -84,15 +84,80 @@ func dsn(dbName string) string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
 }
 
-func main() {
-	fmt.Println("Starting Up")
-
+func dbConnection() (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn(""))
 	if err != nil {
 		log.Printf("Error %s when opening DB\n", err)
+		return nil, err
+	}
+	//defer db.Close()
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	res, err := db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
+	if err != nil {
+		log.Printf("Error %s when creating DB\n", err)
+		return nil, err
+	}
+	no, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when fetching rows", err)
+		return nil, err
+	}
+	log.Printf("rows affected %d\n", no)
+
+	db.Close()
+	db, err = sql.Open("mysql", dsn(dbname))
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return nil, err
+	}
+	//defer db.Close()
+
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(20)
+	db.SetConnMaxLifetime(time.Minute * 5)
+
+	ctx, cancelfunc = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Printf("Errors %s pinging DB", err)
+		return nil, err
+	}
+	log.Printf("Connected to DB %s successfully\n", dbname)
+	return db, nil
+}
+
+func createFileTable(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS files(file_id int primary key auto_increment, product_name text, 
+        product_price int, created_at datetime default CURRENT_TIMESTAMP, updated_at datetime default CURRENT_TIMESTAMP)`
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	res, err := db.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when creating product table", err)
+		return err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when getting rows affected", err)
+		return err
+	}
+	log.Printf("Rows affected when creating table: %d", rows)
+	return nil
+}
+
+func main() {
+	fmt.Println("DupeFinder Starting Up")
+
+	db, err := dbConnection()
+	if err != nil {
+		log.Printf("Error %s when getting db connection", err)
 		return
 	}
 	defer db.Close()
+	log.Printf("Successfully connected to database")
 
 	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
@@ -116,18 +181,14 @@ func main() {
 	}
 	defer db.Close()
 
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(20)
-	db.SetConnMaxLifetime(time.Minute * 5)
-
-	ctx, cancelfunc = context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelfunc()
-	err = db.PingContext(ctx)
+	log.Printf("Successfully connected to database")
+	err = createFileTable(db)
 	if err != nil {
-		log.Printf("Errors %s pinging DB", err)
+		log.Printf("Create files table failed with error %s", err)
 		return
 	}
-	log.Printf("Connected to DB %s successfully\n", dbname)
+
+	fmt.Println("DupeFinder Completed Setting Up Database")
 
 	var inputVar string
 
